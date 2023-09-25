8cad03e7e30eed77a4ce3ac164a6691b25699bba
youfoundjerry <jerryjoekaizer@gmail.com>
2023-09-25 02:31:59 +0100

    not Uber Eats

---
+++ go.mod
@@ -0,0 +1,3 @@
+module Waffle
+
+go 1.21.1
---
+++ waffle.go
@@ -0,0 +1,13 @@
+package waffle 
+
+func Bill () string {
+	return "Your bill is $11.99c"
+}
+
+func Order () string {
+	return "Order is on its way"
+}
+
+func Ready () string {
+	return "Delivered!"
+}
