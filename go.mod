6f494fabe1cc887473b8a15e856be14ba72d6a01
youfoundjerry <jerryjoekaizer@gmail.com>
2023-10-03 12:04:18 +0100

    nothing here

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/iamTruthy/Waffle
 
 go 1.21.1
 
-require github.com/iamTruthy/Coffee v0.0.0-20230926232612-fd1b59451c84 // indirect
+require github.com/iamTruthy/Coffee v0.0.0-20230926232612-fd1b59451c84 // direct
--- waffle.go
+++ waffle.go
@@ -1,18 +1,17 @@
-package waffle 
+package waffle
 
 import (
-  "github.com/iamTruthy/Coffee"
+	"github.com/iamTruthy/Coffee"
 )
 
-func Order () string {
+func Order() string {
 	return "Waffles with Honey"
 }
 
-func Bill () string {
-	return "Your bill is $50.99c"
+func Meal() string {
+	return coffee.WithLatte(Order() + " and a cup of Latte")
 }
 
-
-func Meal () string {
- return coffee.WithLatte("You Ordered " + Order() + " and a cup of Latte")
+func Bill() string {
+	return "Your bill is $50.99c"
 }
